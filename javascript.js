947fc95dd4fda152b0da87281e6763f3fbd1882b
Jackson Zielinski <jackpurrin@proton.me>
2024-09-09 19:24:26 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
+        let blink =
+            document.getElementById('blink');
 
+        setInterval(function () {
+            blink.style.opacity =
+                (blink.style.opacity == 0 ? 1 : 0);
+        }, 1000); 
