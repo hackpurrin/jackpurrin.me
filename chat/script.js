6297ceb2bb576e2c303e7bf32ffc9d5bda584d71
Jackson Zielinski <jackpurrin@proton.me>
2024-09-20 17:14:20 -0700

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -129,3 +129,16 @@ function addMessageToListDOM(text, member) {
     el.scrollTop = el.scrollHeight - el.clientHeight;
   }
 }
+
+        function lightMode() {
+            let element = document.body;
+            let content = document.getElementById("DarkModetext");
+            element.className = "light-mode";
+            content.innerText = "Dark Mode is OFF";
+        }
+        function darkMode() {
+            let element = document.body;
+            let content = document.getElementById("DarkModetext");
+            element.className = "dark-mode";
+            content.innerText = "Dark Mode is ON";
+        }
