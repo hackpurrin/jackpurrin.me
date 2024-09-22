f695ad57e2e9f8a4d1db7b4fffadd7364424ddd3
Jackson Zielinski <jackpurrin@proton.me>
2024-09-21 19:43:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ document.body.className = 'light-mode';
 function darkMode() {
 document.body.className = 'dark-mode';
 }
+
+function printRecipt() {
+console.log("==================================")
+console.log("         McDONALD'S RECEIPT       ")
+console.log("----------------------------------")
+console.log("           Order Details          ")
+console.log("----------------------------------")
+console.log("Item           Quantity   Price")
+console.log("----------------------------------")
+console.log("🍔 Burger       1          2.99   ")
+console.log("🍟 Fries        1          2.49   ")
+console.log("🥤 Soda         1          1.99   ")
+console.log("----------------------------------")
+console.log("Total                      7.47   ")
+console.log("==================================")
+}
