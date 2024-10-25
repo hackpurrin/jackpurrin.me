385864a788c18a4633282329cd2ccaef44220be8
jackpurrin <jackpurrin@proton.me>
2024-10-24 18:31:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function IECheck()
-{
-    if (ieVersion < 9) 
-    {
-        document.getElementById("div1").style.display = "block";
-    }
-}
+var $buoop = {required:{e:-6,f:-6,o:-6,s:-1,c:-6},insecure:true,unsupported:true,api:2024.10 }; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.min.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
