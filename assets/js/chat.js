1d428abf86affeaa456004915af55f6a819809e6
jackpurrin <jackpurrin@proton.me>
2024-12-11 16:06:23 -0700

    fixes the chat room.

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -57,9 +57,12 @@ drone.on('error', error => {
 });
 
 function getRandomName() {
+  const heart = ["❤️", "🧡", "💛", "💚", "💙", "💜", "💘" ]
   const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
   const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
   return (
+    heart[Math.floor(Math.random() * heart.length)] +
+    " " +
     adjs[Math.floor(Math.random() * adjs.length)] +
     "_" +
     nouns[Math.floor(Math.random() * nouns.length)]
@@ -67,7 +70,7 @@ function getRandomName() {
 }
 
 function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  return '#' + Math.floor(Math.random() * 0xFF69FF).toString(16);
 }
 
 //------------- DOM STUFF
